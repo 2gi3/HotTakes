e949230a19645fb305100727cf54232d7906fb6d
2gi3 <gippolito@hotmail.co.uk>
2022-02-26 16:43:22 +0000

    authentication tokens

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -2,11 +2,12 @@ const Sauce = require('../models/sauce');
 
 exports.createSauce= (req, res, next) => {
     const sauce = new Sauce({
-      title: req.body.title,
+      name: req.body.name,
+      manifacturer: req.body.manifacturer,
       description: req.body.description,
       imageUrl: req.body.imageUrl,
-      price: req.body.price,
-      userId: req.body.userId
+      mainPepperIngredient: req.body. mainPepperIngredient,
+      heat: req.body.heat
     });
     sauce.save().then(
       () => {
@@ -53,18 +54,32 @@ exports.createSauce= (req, res, next) => {
     );
   };
 
-  exports.deleteOneSauce =(req, res, next) => {
-    Sauce.deleteOne({_id: req.params.id}).then(
-      () => {
-        res.status(200).json({
-          message: 'Deleted!'
-        });
-      }
-    ).catch(
-      (error) => {
-        res.status(400).json({
-          error: error
-        });
+  exports.deleteOneSauce = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id }).then(
+      (sauce) => {
+        if (!sauce) {
+          return res.status(404).json({
+            error: new Error('Objet non trouvé !')
+          });
+        }
+        if (sauce.userId !== req.auth.userId) {
+          return res.status(401).json({
+            error: new Error('Requête non autorisée !')
+          });
+        }
+        sauce.deleteOne({_id: req.params.id}).then(
+          () => {
+            res.status(200).json({
+              message: 'Deleted!'
+            });
+          }
+        ).catch(
+          (error) => {
+            res.status(400).json({
+              error: error
+            });
+          }
+        );
       }
     );
   };
--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -1,11 +1,11 @@
 const express = require('express');
-
+const auth = require('../middleware/auth');
 const router = express.Router();
 const saucesCtrl = require('../controllers/sauces');
 
-  router.post('/', saucesCtrl.createSauce);  
-  router.get('/', saucesCtrl.getAllSauces);
-  router.get('/:id', saucesCtrl.getOneSauce);
-  router.delete('/:id', saucesCtrl.deleteOneSauce);
+  router.post('/', auth, saucesCtrl.createSauce);  
+  router.get('/', auth, saucesCtrl.getAllSauces);
+  router.get('/:id', auth, saucesCtrl.getOneSauce);
+  router.delete('/:id', auth, saucesCtrl.deleteOneSauce);
 
 module.exports = router;
