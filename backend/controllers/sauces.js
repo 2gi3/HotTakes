4de462e2052ff0707f266624f6ab1cbd8203de7d
2gi3 <gippolito@hotmail.co.uk>
2022-02-27 23:48:08 +0000

    delete sauce

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -1,15 +1,16 @@
 const Sauce = require('../models/sauce');
+const fs = require('fs');
 
 exports.createSauce= (req, res, next) => {
   req.body.sauce = JSON.parse(req.body.sauce);
   const url = req.protocol + '://' + req.get('host');
     const sauce = new Sauce({
-      name: req.body.name,
-      manifacturer: req.body.manifacturer,
-      description: req.body.description,
+      name: req.body.sauce.name,
+      manifacturer: req.body.sauce.manifacturer,
+      description: req.body.sauce.description,
       imageUrl: url + '/images/' + req.file.filename,
-      mainPepperIngredient: req.body. mainPepperIngredient,
-      heat: req.body.heat
+      mainPepperIngredient: req.body.sauce. mainPepperIngredient,
+      heat: req.body.sauce.heat
     });
     sauce.save().then(
       () => {
@@ -62,21 +63,21 @@ exports.createSauce= (req, res, next) => {
       const url = req.protocol + '://' + req.get('host');
       req.body.sauce = JSON.parse(req.body.sauce);
       sauce = {
-        name: req.body.name,
-        manifacturer: req.body.manifacturer,
-        description: req.body.description,
-        imageUrl: url + '/images/' + req.file.filename,
-        mainPepperIngredient: req.body. mainPepperIngredient,
-        heat: req.body.heat
+        name: req.body.sauce.name,
+      manifacturer: req.body.sauce.manifacturer,
+      description: req.body.sauce.description,
+      imageUrl: url + '/images/' + req.file.filename,
+      mainPepperIngredient: req.body.sauce. mainPepperIngredient,
+      heat: req.body.sauce.heat
       };
     } else {
       sauce = {
-        name: req.body.name,
-      manifacturer: req.body.manifacturer,
-      description: req.body.description,
+        name: req.body.sauce.name,
+      manifacturer: req.body.sauce.manifacturer,
+      description: req.body.sauce.description,
       imageUrl: req.body.imageUrl,
-      mainPepperIngredient: req.body. mainPepperIngredient,
-      heat: req.body.heat
+      mainPepperIngredient: req.body.sauce. mainPepperIngredient,
+      heat: req.body.sauce.heat
       };
     }
     sauce.updateOne({_id: req.params.id}, sauce).then(
@@ -96,31 +97,24 @@ exports.createSauce= (req, res, next) => {
 
 
   exports.deleteOneSauce = (req, res, next) => {
-    Sauce.findOne({ _id: req.params.id }).then(
+    Sauce.findOne({_id: req.params.id}).then(
       (sauce) => {
-        if (!sauce) {
-          return res.status(404).json({
-            error: new Error('Objet non trouvé !')
-          });
-        }
-        if (sauce.userId !== req.auth.userId) {
-          return res.status(401).json({
-            error: new Error('Requête non autorisée !')
-          });
-        }
-        sauce.deleteOne({_id: req.params.id}).then(
-          () => {
-            res.status(200).json({
-              message: 'Deleted!'
-            });
-          }
-        ).catch(
-          (error) => {
-            res.status(400).json({
-              error: error
-            });
-          }
-        );
+        const filename = sauce.imageUrl.split('/images/')[1];
+        fs.unlink('images/' + filename, () => {
+          Sauce.deleteOne({_id: req.params.id}).then(
+            () => {
+              res.status(200).json({
+                message: 'Deleted!'
+              });
+            }
+          ).catch(
+            (error) => {
+              res.status(400).json({
+                error: error
+              });
+            }
+          );
+        });
       }
     );
   };
